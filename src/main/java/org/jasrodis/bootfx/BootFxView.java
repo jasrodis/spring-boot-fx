deb0bb942614dc380e3c0fdf8a389ffb76db11ae
Iason Rodis <iason-dimitrios.rodis@cern.ch>
2018-05-04 17:46:32 +0200

    Removed fxml, has issues will add it from branch later on

--- src/main/java/org/jasrodis/bootfx/BootFxView.java
+++ src/main/java/org/jasrodis/bootfx/BootFxView.java
@@ -1,7 +1,6 @@
 package org.jasrodis.bootfx;
 
 import org.jasrodis.bootfx.gui.chart.ChartView;
-import org.jasrodis.bootfx.gui.form.fxml.FormFxmlController;
 import org.jasrodis.bootfx.gui.form.programmatic.FormView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,14 +19,12 @@ public class BootFxView extends BorderPane {
 
 	private final ChartView chartView;
 	private final FormView formView;
-	private final FormFxmlController formFxmlController;
 
 	TabPane tabPane = new TabPane();
 
-	public BootFxView(ChartView chartView, FormView formView, FormFxmlController formFxmlController) throws Exception {
+	public BootFxView(ChartView chartView, FormView formView) {
 		this.chartView = chartView;
 		this.formView = formView;
-		this.formFxmlController = formFxmlController;
 
 		Platform.runLater(() -> {
 			log.info("Initializing  [{}] ", getClass().getSimpleName());
@@ -42,12 +39,7 @@ public class BootFxView extends BorderPane {
 			formTab.setContent(formView);
 			formTab.setClosable(false);
 
-			Tab formFxmlTab = new Tab();
-			formFxmlTab.setText(formFxmlController.getViewAsParent().getUserData().toString());
-			formFxmlTab.setContent(formFxmlController.getViewAsParent());
-			formFxmlTab.setClosable(false);
-
-			tabPane.getTabs().addAll(chartTab, formTab, formFxmlTab);
+			tabPane.getTabs().addAll(chartTab, formTab);
 			setCenter(tabPane);
 		});
 	}
